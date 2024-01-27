7088496b3ef4f67681b9f25c214487fdb6925a08
Parth-Kolekar <parthkolekar9@gmail.com>
2024-01-27 23:25:45 +0530

    Game window created

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print('hello pygame')
+import pygame
+from sys import exit
+
+pygame.init()
+screen=pygame.display.set_mode((800,500))
+pygame.display.set_caption('VSGP MARIO LITE')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #draw all our elements
+    #update every fucking thing
+    pygame.display.update()
