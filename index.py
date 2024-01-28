5709c4f9a0fb4199e30e5b6ab0d6a1da77d987f3
Parth-Kolekar <parthkolekar9@gmail.com>
2024-01-28 14:10:33 +0530

    Game window created

--- index.py
+++ index.py
@@ -1,9 +1,14 @@
 import pygame
-from sys import exit
+import sys #pygame. font. SysFont()
 
 pygame.init()
 screen=pygame.display.set_mode((800,500))
 pygame.display.set_caption('VSGP MARIO LITE')
+clock = pygame.time.Clock()
+test_font = pygame. font. SysFont(None,50)
+
+sky_surface = pygame.image.load('Graphics/gb3.jpg')
+text_surface = test_font.render('Lord Gaurav',False,"cyan")
 
 while True:
     for event in pygame.event.get():
@@ -11,6 +16,9 @@ while True:
             pygame.quit()
             exit()
 
+    screen.blit(sky_surface,(0,0))
+    screen.blit(text_surface,(300,50))
     #draw all our elements
     #update every fucking thing
     pygame.display.update()
+    clock.tick(60)
