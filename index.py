b015e276485eda1e0ce51f7e043707a4879f7455
Parth-Kolekar <parthkolekar9@gmail.com>
2024-01-28 15:53:08 +0530

    new font applied

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import pygame
-import sys #pygame. font. SysFont()
+import sys
 
 pygame.init()
 screen=pygame.display.set_mode((800,500))
 pygame.display.set_caption('VSGP MARIO LITE')
 clock = pygame.time.Clock()
-test_font = pygame. font. SysFont(None,50)
-
+test_font = pygame. font. Font('Font/ARCADECLASSIC.TTF',50) #small font - size:30 ,x=300
+                                                                       # large font - size:50 ,x=265
 sky_surface = pygame.image.load('Graphics/gb3.jpg')
 text_surface = test_font.render('Lord Gaurav',False,"cyan")
 
@@ -17,7 +17,7 @@ while True:
             exit()
 
     screen.blit(sky_surface,(0,0))
-    screen.blit(text_surface,(300,50))
+    screen.blit(text_surface,(265,50))
     #draw all our elements
     #update every fucking thing
     pygame.display.update()
