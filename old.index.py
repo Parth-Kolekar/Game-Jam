e41e2b1b552f236f142af3107e19150e226e4273
shubhamkarkhanis <shubhamkarkhanis2005@gmail.com>
2024-02-08 21:35:17 +0530

    old game window size increased

--- old.index.py
+++ old.index.py
@@ -2,15 +2,15 @@ import pygame
 import sys
                #THIS WAS TRIAL WINDOW
 pygame.init()
-screen=pygame.display.set_mode((800,500))
-pygame.display.set_caption('VSGP MARIO LITE')
+screen=pygame.display.set_mode((1600,900))
+pygame.display.set_caption('TRIAL WINDOW')
 clock = pygame.time.Clock()
-test_font = pygame. font. Font('Font/ARCADECLASSIC.TTF',50) #small font - size:30 ,x=300
+test_font = pygame. font. Font('Font/ARCADECLASSIC.TTF',100) #small font - size:30 ,x=300
                                                                        # large font - size:50 ,x=295
-sky_surface = pygame.image.load('Graphics/Battleground3Resize.jpg').convert()
+sky_surface = pygame.image.load('Graphics/Battleground3.png').convert()
 text_surface = test_font.render('Game Jam',False,"cyan")
 
-hero_one = pygame.image.load('Graphics/Sprites/run5.png').convert_alpha() # hero size 50 x70
+hero_one = pygame.image.load('Graphics/Sprites/run5_new.png').convert_alpha() # hero size 50 x70
 hero_x_pos =0
 
 while True:
@@ -20,13 +20,11 @@ while True:
             exit()
 
     screen.blit(sky_surface,(0,0))
-    screen.blit(text_surface,(295,50))
+    screen.blit(text_surface,(595,50))
     hero_x_pos +=4
-    if hero_x_pos > 900: hero_x_pos = 0    #hero runs to end and starts again
-    screen.blit(hero_one,(hero_x_pos,300))
+    if hero_x_pos > 1500: hero_x_pos = -300    #hero runs to end and starts again
+    screen.blit(hero_one,(hero_x_pos,280))
 
-    #I am here
-    #draw all our elements
-    #update every fucking thing
+    #update everything
     pygame.display.update()
     clock.tick(60)
