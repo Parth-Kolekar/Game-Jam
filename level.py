eb2c3c9fdd132b496d98858d793c4e45d51742d9
shubhamkarkhanis <shubhamkarkhanis2005@gmail.com>
2024-02-12 22:26:14 +0530

    player camera centered, old files cleaned up

--- level.py
+++ level.py
@@ -57,10 +57,10 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < screen_width/4 and direction_x < 0:
+        if player_x < screen_width/2 and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
-        elif player_x > screen_width - (screen_width/4) and direction_x > 0:
+        elif player_x > screen_width - (screen_width/2) and direction_x > 0:
             self.world_shift = -8
             player.speed = 0
         else:
