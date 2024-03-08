ff138a6b0d7d9e28cd06f109b44275f6db6244a5
shubhamkarkhanis <shubhamkarkhanis2005@gmail.com>
2024-03-08 20:26:54 +0530

    changed background

--- main.py
+++ main.py
@@ -8,11 +8,28 @@ pygame.init()
 pygame.display.set_caption('Game Jam')
 
 #add ",pygame.FULLSCREEN" for fullscreen mode
-screen = pygame.display.set_mode((screen_width,screen_height),pygame.FULLSCREEN)
+screen = pygame.display.set_mode((screen_width,screen_height), pygame.FULLSCREEN)
 
-background_img = pygame.image.load('graphics/background/windrise-background-4k.png').convert()
+background_img = pygame.image.load('graphics/background/layers/grassy_mountain_combined.png').convert()
 scaled_background = pygame.transform.scale(background_img, (screen_width, screen_height))
 
+#### Unused code for scrolling parallax backgorund #########################
+bg_images = []
+for i in range (1,6):
+    bg_image = pygame.image.load(f"graphics/background/glacial_mountain/bg_layer_{i}.png").convert_alpha()
+    scaled_bg = pygame.transform.scale(bg_image, (screen_width, screen_height))
+    bg_images.append(scaled_bg)
+
+bg_width = bg_images[0].get_width()
+
+def draw_bg(bg_scroll):
+    for x in range(5):
+        speed = 1
+        for i in bg_images:
+            screen.blit(i, ((x * bg_width) - bg_scroll * speed ,0))
+            speed += 0.5
+############################################################################
+            
 clock = pygame.time.Clock()
 level = Level(level_0, screen)
 
@@ -22,6 +39,8 @@ while True:
             pygame.quit()
             sys.exit()
     screen.blit(scaled_background,(0,0))
+    #bg_scroll = level.world_shift   #unused for scrolling background
+    #draw_bg(bg_scroll)              #unused for scrolling background
     level.run()
 
     pygame.display.update()
